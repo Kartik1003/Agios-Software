16649d1f4c7943a8246276a1589a5f9f38ced701
Kartik1003 <jaiswalkartikey559@gmail.com>
2026-05-30 16:18:03 +0530

    Configure production API

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,7 +1,7 @@
 // client/src/utils/api.js
 // API client for communicating with the Agios backend
 
-const API_BASE = 'http://localhost:3002/api';
+const API_BASE = 'import.meta.env.VITE_API_URL';
 
 /**
  * Start a new compilation
